9467e3a20dfee39c527213468141370e521ea096
ib220 <76448606+ib220@users.noreply.github.com>
2023-02-03 16:41:00 +0000

    Update 2010Concentration.py
    Have made changes to plots

--- 2010Concentration.py
+++ 2010Concentration.py
@@ -1,98 +1,78 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan 29 22:40:56 2023
-
-@author: ioanabalabasciuc
-"""
+#You can always go ahead and make changes to colorbars and other plotting variables
+# to try to ake the figure look either prettier, or  more informative.
 
 import iris
 import numpy as np
 import matplotlib.pyplot as plt
-import cftime
-import iris.plot as iplt
-import iris.quickplot as qplt
-import cartopy.crs as ccrs
 
 #loading data
-
 CO14_data = iris.load_cube('2010CO14.nc')
+CO14_prod = iris.load('CO14Production2010.nc')[0]
+#CO14_loss = iris.load_cube('OHCO14reaction.nc')
 
-if not CO14_data.coord('latitude').has_bounds():
-    CO14_data.coord('latitude').guess_bounds()
-    CO14_data.coord('longitude').guess_bounds()
-    
+#%%
+#looking at height variation 
 
-#%% Surface 
+#plotting heights at centre of each cell 
+level_heights = CO14_data.coord('level_height').points
+plt.figure()
+plt.plot(level_heights, 'x')
+plt.xlabel('Model Level Height')
+plt.ylabel('Altitude (m)')
 
-#CO14_data_surface = CO14_data.extract(iris.Constraint(model_level_number = lambda cell: cell == 1))
+#finding bounds for each cell - thickness non unifrom greater at top of model 
+level_thickness = CO14_data.coord('level_height').bounds[:,1] -  CO14_data.coord('level_height').bounds[:,0]
+plt.figure()
+plt.plot(level_thickness, 'x')
+plt.xlabel('Model Level Height')
+plt.ylabel('Model Level Thickness (m)')
 
-# coords_lt = ['longitude','time']
-# data_mean = CO14_data_surface.collapsed(coords_lt,iris.analysis.MEAN, 
-#                             weights = iris.analysis.cartography.area_weights(CO14_data_surface))
-# plt.plot(data_mean.data.data)
-# plt.ylabel('$^{14}$CO Concentration (molecule cm$^{-3}$)')
-# plt.xlabel('Latitude box #')
-# plt.title('Average Annual change with latitude for mean longitude')
-# plt.show()
+
+#%%
 
 
 def concen_month(data,monthspec):
+    """
+    Function collpases cube over longitude and time, 
+    returning mean 14CO concentration
+    """
     data_month = data.extract(iris.Constraint(time = lambda cell: cell.point.month == monthspec))
     coords_lt = ['longitude','time']
-    data_mean = data_month.collapsed(coords_lt,iris.analysis.MEAN, 
-                               weights = iris.analysis.cartography.area_weights(data_month))
-
+    data_mean = data_month.collapsed(coords_lt,iris.analysis.MEAN)
     
     return data_mean
 
 
-#creating 3d maps 
+
+#plotting 14CO concentration for each month and saving data
 for m in range(1,13): 
     data_months = concen_month(CO14_data,m)
-    plt.imshow(data_months.data.data.data)
+    #saving data as nc file
+    iris.save(data_months,'ConcData/14COcon{}.nc'.format(m) )
+    
+    #extracting latitude at mindpoint of each cell 
+    latitude = data_months.coord('latitude').points
+    #extracting altitude at mindpoint of each cell  in km 
+    altitude = (data_months.coord('level_height').points)/1000
+    
+    #defining the tropopause level - only works for pressure plot 
+    Tropopause = (300 - 215 * (np.cos((np.pi * latitude)/180)) ** 2)/1000
+    
+    #using first order approximation to find pressure 
+    pressure = 1000 * np.exp( -altitude / 8.5e3)
+    
+    #plt.pcolor(latitude,altitude,data_months.data) # plotting latitude 
+    plt.pcolor(latitude,pressure,data_months.data) #plotting presusre 
+    plt.gca().invert_yaxis() # invert axis as max pressure at surface 
+    
+    plt.plot(latitude,Tropopause) # plot approx tropopause only for pressure
+    
+    #plt.yticks([10,100,200,300,400,500,600,700,800,900,1000])
+
+    cbar = plt.colorbar()
+    cbar.set_label('$^{14}$CO Concentration (molecules cm$^{-3}$)')
     plt.title('14C concentration in month #{}'.format(m))
     plt.xlabel('Latitude')
-    plt.ylabel('Altitude')
-    plt.colorbar()
-    plt.savefig('14C_con_long{}.png'.format(m))
+    plt.ylabel('Altitude (km)')
+    plt.savefig('ConcPlot/14C_con_long{}.png'.format(m))
     plt.show()
-#in the console. This should be a 360 x 85 x 144 x 192. Because of the way these netCDF files
-
-
-
-#%%
-for j in range(1,10):
-    #print('j',j)
-    
-    CO14_data_surface = CO14_data.extract(iris.Constraint(model_level_number = lambda cell: cell == j))
-   
-    surface = np.zeros(shape=(144,13))
-
-    for m in range(1,13): 
-        #print('m',m)
-        data_months = concen_month(CO14_data_surface,m)
-        surface[:,m] = data_months.data.data
-
-    plt.plot(surface[:,1])
-    plt.plot(surface[:,2])
-    plt.plot(surface[:,3])
-    plt.plot(surface[:,4])                              
-    plt.plot(surface[:,5])
-    plt.plot(surface[:,6])
-    plt.plot(surface[:,7])
-    plt.plot(surface[:,8])
-    plt.plot(surface[:,9])
-    plt.plot(surface[:,10])
-    plt.plot(surface[:,11])
-    plt.plot(surface[:,12])
-
-    plt.ylabel('$^{14}$CO Concentration (molecule cm$^{-3}$)')
-    plt.xlabel('Longitude box #')
-    plt.title('Change with latitude for mean longitude, Cell= {}'.format(j))
-    
-    plt.legend(['1','2','3','4','5','6','7','8','9','10','11','12'],loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.show()
-
-    #total[:,j:j+12]
-    
