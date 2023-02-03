14b05c01ae17a6db198201273c668ddca4d6b036
ib220 <76448606+ib220@users.noreply.github.com>
2023-02-03 16:47:48 +0000

    Update 2010Concentration.py
    Have made changes to plots

--- 2010Concentration.py
+++ 2010Concentration.py
@@ -19,6 +19,7 @@ plt.figure()
 plt.plot(level_heights, 'x')
 plt.xlabel('Model Level Height')
 plt.ylabel('Altitude (m)')
+plt.show()
 
 #finding bounds for each cell - thickness non unifrom greater at top of model 
 level_thickness = CO14_data.coord('level_height').bounds[:,1] -  CO14_data.coord('level_height').bounds[:,0]
@@ -26,6 +27,7 @@ plt.figure()
 plt.plot(level_thickness, 'x')
 plt.xlabel('Model Level Height')
 plt.ylabel('Model Level Thickness (m)')
+plt.show()
 
 
 #%%
@@ -53,10 +55,10 @@ for m in range(1,13):
     #extracting latitude at mindpoint of each cell 
     latitude = data_months.coord('latitude').points
     #extracting altitude at mindpoint of each cell  in km 
-    altitude = (data_months.coord('level_height').points)/1000
+    altitude = (data_months.coord('level_height').points)
     
     #defining the tropopause level - only works for pressure plot 
-    Tropopause = (300 - 215 * (np.cos((np.pi * latitude)/180)) ** 2)/1000
+    Tropopause = (300 - 215 * (np.cos((np.pi * latitude)/180)) ** 2)
     
     #using first order approximation to find pressure 
     pressure = 1000 * np.exp( -altitude / 8.5e3)
@@ -65,14 +67,18 @@ for m in range(1,13):
     plt.pcolor(latitude,pressure,data_months.data) #plotting presusre 
     plt.gca().invert_yaxis() # invert axis as max pressure at surface 
     
-    plt.plot(latitude,Tropopause) # plot approx tropopause only for pressure
+    plt.plot(latitude,Tropopause, color='red', label='Tropopause') # plot approx tropopause only for pressure
     
-    #plt.yticks([10,100,200,300,400,500,600,700,800,900,1000])
+    plt.yticks([10,100,200,300,400,500,600,700,800,900,1000])
 
     cbar = plt.colorbar()
     cbar.set_label('$^{14}$CO Concentration (molecules cm$^{-3}$)')
     plt.title('14C concentration in month #{}'.format(m))
     plt.xlabel('Latitude')
-    plt.ylabel('Altitude (km)')
+    plt.ylabel('Pressure (hbar)')
+   #plt.ylabel('Altitude (km)')
+    plt.legend(loc='lower left')
     plt.savefig('ConcPlot/14C_con_long{}.png'.format(m))
     plt.show()
+
+
