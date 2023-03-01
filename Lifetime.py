e9b6d046a0e67548d0eb80534583e89de8b62489
ib220 <76448606+ib220@users.noreply.github.com>
2023-03-01 20:53:08 +0000

    At given surface and over all altitudes

--- Lifetime.py
+++ Lifetime.py
@@ -9,152 +9,218 @@ import iris
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
-#loading data
-CO14_data = iris.load_cube('2010CO14.nc') #units mol/cm^3
+import matplotlib.colors as mcolors
+
+
+CO14_08 = iris.load_cube('Concentration/CO142008.nc') #units mol/cm^3
+CO14_09 = iris.load_cube('Concentration/CO142009.nc') #units mol/cm^3
+CO14_10 = iris.load_cube('Concentration/CO142010.nc') #units mol/cm^3
+CO14_11 = iris.load_cube('Concentration/CO142011.nc') #units mol/cm^3
+CO14_12 = iris.load_cube('Concentration/CO142012.nc') #units mol/cm^3
+
+
+tropo08 = iris.load_cube('Tropopause/2008Tropopause.nc')
+tropo09 = iris.load_cube('Tropopause/2009Tropopause.nc')
+tropo10 = iris.load_cube('Tropopause/2010Tropopause.nc')
+tropo11 = iris.load_cube('Tropopause/2011Tropopause.nc')
+tropo12 = iris.load_cube('Tropopause/2012Tropopause.nc')#m
 
 #loading production files for all years 
-CO14_prod10 = iris.load('CO14Production2010.nc')[0] #units kg/m2/s
-CO14_prod11 = iris.load('CO14Production2011.nc')[0] #units kg/m2/s
-CO14_prod12 = iris.load('CO14Production2012.nc')[0] #units kg/m2/s
+CO14_prod08 = iris.load('Production/CO14Production2008.nc')[0] #units kg/m2/s
+CO14_prod09 = iris.load('Production/CO14Production2009.nc')[0] #units kg/m2/s
+CO14_prod10 = iris.load('Production/CO14Production2010.nc')[0] #units kg/m2/s
+CO14_prod11 = iris.load('Production/CO14Production2011.nc')[0] #units kg/m2/s
+CO14_prod12 = iris.load('Production/CO14Production2012.nc')[0] #units kg/m2/s
 
 #loading loss files for all years 
-CO14_loss10 = iris.load_cube('OHCO142010reaction.nc')  #mol/gridcell/s
-CO14_loss11 = iris.load_cube('OHCO142011reaction.nc')  #mol/gridcell/s
-CO14_loss12 = iris.load_cube('OHCO142012reaction.nc')  #mol/gridcell/s
+CO14_loss08 = iris.load_cube('Loss/2008Loss.nc')  #mol/gridcell/s
+CO14_loss09 = iris.load_cube('Loss/2009Loss.nc')  #mol/gridcell/s
+CO14_loss10 = iris.load_cube('Loss/2010Loss.nc')  #mol/gridcell/s
+CO14_loss11 = iris.load_cube('Loss/2011Loss.nc')  #mol/gridcell/s
+CO14_loss12 = iris.load_cube('Loss/2012Loss.nc')  #mol/gridcell/s
 
+tropo08 = iris.load_cube('Tropopause/2008Tropopause.nc')
+tropo09 = iris.load_cube('Tropopause/2009Tropopause.nc')
+tropo10 = iris.load_cube('Tropopause/2010Tropopause.nc')
+tropo11 = iris.load_cube('Tropopause/2011Tropopause.nc')
+tropo12 = iris.load_cube('Tropopause/2012Tropopause.nc')#m
 
-tropo = iris.load_cube('2010Tropopause.nc')  #m
+vol_model08 = iris.load('Volume/2008Volume.nc')[0] #m3
+vol_model09 = iris.load('Volume/2009Volume.nc')[0]#m3
+vol_model10 = iris.load('Volume/2010Volume.nc')[0] #m3
+vol_model11 = iris.load('Volume/2011Volume.nc')[0] #m3
+vol_model12 = iris.load('Volume/2012Volume.nc')[0] #m3
 
 #%%
+kg_vol = []
+for monthspec in range(1,13):
+    data_month = CO14_10.extract(iris.Constraint(time = lambda cell: cell.point.month == monthspec))
+    coords_lt = ['time']
+    data_mean = data_month.collapsed(coords_lt,iris.analysis.MEAN)
+    col_month = vol_model08.extract(iris.Constraint(time = lambda cell: cell.point.month == monthspec))
+    
+    CO14 = data_mean * col_month *1e6
+    netsum = CO14.collapsed(['latitude','longitude','model_level_number'],iris.analysis.SUM).data 
+    net_sum_new =  netsum*(1/6.02e23)*0.03
+    kg_vol.append(net_sum_new)
+    
+
+print('Total_concentration', sum(kg_vol)/12)
+
+#%% Converting Production from kg/m2/s to mol/m3/s
 #height at each grid cell 
-h = CO14_data.coord('level_height').bounds[:,1]
+h = CO14_10.coord('level_height').bounds[:,1]
 
 ###### Determining conversion factors #########
 # mol to mass (kg) 
 kg_mol = 1/30e-3 
 #altitude of each cell
-level_thick = CO14_data.coord('level_height').bounds[:,1] -  CO14_data.coord('level_height').bounds[:,0]
-print(np.shape(level_thick))
+level_thick = CO14_10.coord('level_height').bounds[:,1] -  CO14_10.coord('level_height').bounds[:,0]
 #CONVERSION FACTOR FOR PRODUCTION
 prod_con = np.zeros([85,144])
-prod_conv1 = kg_mol/(level_thick*10e6) #dividing by cell height and converting from m^3 to cm^3
+prod_conv1 = kg_mol/(level_thick*1e6) #dividing by cell height and converting from m^3 to cm^3
 
 for i in range(0,85):
     prod_con[i,:] = prod_conv1[i]
 
 prod_conv = prod_con
 
-#CONVERSION FACTOR FOR LOSS 
-
-#finding box area
-weights=iris.analysis.cartography.area_weights(CO14_loss10)
-area = weights[0,:,:,0]
-print(np.shape(area))
-#creating (85,144) array for height 
-
-heights = np.zeros([85,144])
-for i in range(0,85):
-   heights[i,:] = level_thick[i]
-   
-
-
-volume = area * heights  
-print(volume[0,0])
-print(volume[0,-1])
-np.savetxt('volume.csv',volume*10e6)
-loss_conv = 1/(volume*10e6) 
-
 def concen_month(data,monthspec):
     """
     Function collpases cube over longitude and time, 
     returning mean 14CO concentration
     """
-    data_month = data.extract(iris.Constraint(time = lambda cell: cell.point.month == monthspec))
+    data_month = data.extract(iris.Constraint(time = lambda cell: cell.point.month == i))
     coords_lt = ['longitude','time']
     data_mean = data_month.collapsed(coords_lt,iris.analysis.MEAN)
 
     return data_mean 
 
-plt.plot(volume[0,:])
-plt.yscale('log')
-
-#%% Calcualting chemical lifetime 
-for i in range(1,2):
-
-    rloss = concen_month(CO14_loss12,i)
-    rconc = concen_month(CO14_data,i)
-
-    rloss.units = 'kg m-2 s-1'
-    #converting production - loss into mole/cm3/month
-    loss_months1 = (rloss.data*loss_conv)*6.02e23*(365*24*60*60)
-    np.savetxt('lossJan.csv',rloss)
-    np.savetxt('lossJanconv.csv', loss_months1)
-    #obtaining concentration at month 
-    data_months = np.divide(rconc.data,loss_months1)
-    #extracting latitude at mindpoint of each cell 
-    latitude = rloss.coord('latitude').points
-    #extracting altitude at mindpoint of each cell  in m 
-
-    ##### NOTE #######
-    #height in concentration is level_height 
-    #height in prod and loss is atmosphere_hybrid_height_coordinate
-    altitude = (rloss.coord('level_height').points)
+#%% Calcualting chemical lifetime  at  surface IN 2010 
+for i in [1,7]:
     
-    plt.figure(figsize = (25,12.5))
-    plt.plot(latitude,loss_months1[65,:], label = 'Loss molecules/cm3/year')
-    plt.plot(latitude,rconc.data[65,:],label='Concentration molecules/cm3')
-    plt.legend()
-    plt.ylim(bottom = 0.0)
+    #obtaining loss for each month at surface 
+    loss_month = CO14_loss11.extract(iris.Constraint(time = lambda cell: cell.point.month == i))
+    loss_surface = loss_month.extract(iris.Constraint(model_level_number= lambda cell: cell == 1))
+    rloss = loss_surface.collapsed('time',iris.analysis.MEAN)
+
+    #obtaining cocentration for each month at surface 
+    concen_month = CO14_11.extract(iris.Constraint(time = lambda cell: cell.point.month == i))
+    concen_surface = concen_month.extract(iris.Constraint(model_level_number= lambda cell: cell == 1))
+    rconc = concen_surface.collapsed('time',iris.analysis.MEAN).data
+    
+    rloss_array = np.array(rloss.data,dtype='float64')
+    #converting production - loss into molecules/cm3/month
+    loss_months1 = (rloss_array*6.02e23*(24*60*60))/(vol_model11.data[i-1,1,:,:]*1e6)
+    
+    #Calculating the chemical lifetime 
+    data_months = rconc/loss_months1
+    
+    #extracting latitude at mindpoint of each cell in m 
+    latitude = rloss.coord('latitude').points
+    longitude = rloss.coord('longitude').points
+    
+    #creating uneven levels 
+    uneven_levels = [10, 15,30, 60, 360]#720,360*3,4*360,5*360]
+    cmap_rb = plt.get_cmap('Spectral')
+    colors = cmap_rb(np.linspace(0, 1, len(uneven_levels) - 1))
+    cmap, norm = mcolors.from_levels_and_colors(uneven_levels, colors)
+
+    plt.figure()
+    cs= plt.pcolormesh(longitude,latitude,data_months,cmap=cmap,norm=norm) # plotting latitude
+
+    cbar= plt.colorbar(cs,ticks=uneven_levels,extend='max')
+   
+    cbar.set_label('Lifetime (day)')
+    plt.title('Chemical lifetime of 14CO in 2011 #{} at 990mb'.format(i))
+    plt.xlabel('Latitude')
+    plt.ylabel('Longitude')
+    plt.savefig('ChemLoss/Surface/14C_chemloss11_{}.png'.format(i))
+    plt.show()
+
+#%% Calcualting chemical lifetime  at  550mb IN 2010 
+for i in [1,7]:
+    
+    #obtaining loss for each month at surface 
+    loss_month = CO14_loss11.extract(iris.Constraint(time = lambda cell: cell.point.month == i))
+    loss_surface = loss_month.extract(iris.Constraint(model_level_number= lambda cell: cell == 29))
+    rloss = loss_surface.collapsed('time',iris.analysis.MEAN)
+
+    #obtaining cocentration for each month at surface 
+    concen_month = CO14_11.extract(iris.Constraint(time = lambda cell: cell.point.month == i))
+    concen_surface = concen_month.extract(iris.Constraint(model_level_number= lambda cell: cell == 29))
+    rconc = concen_surface.collapsed('time',iris.analysis.MEAN).data
+    
+    rloss_array = np.array(rloss.data,dtype='float64')
+    #converting production - loss into molecules/cm3/month
+    loss_months1 = (rloss_array*6.02e23*(24*60*60))/(vol_model11.data[i-1,29,:,:]*1e6)
+    
+    #Calculating the chemical lifetime 
+    data_months = rconc/loss_months1
+    
+    #extracting latitude at mindpoint of each cell in m 
+    latitude = rloss.coord('latitude').points
+    longitude = rloss.coord('longitude').points
+    
+    #creating uneven levels 
+    uneven_levels = [10, 15,30, 60, 360]#720,360*3,4*360,5*360]
+    cmap_rb = plt.get_cmap('Spectral')
+    colors = cmap_rb(np.linspace(0, 1, len(uneven_levels) - 1))
+    cmap, norm = mcolors.from_levels_and_colors(uneven_levels, colors)
+
+    plt.figure()
+    cs= plt.pcolormesh(longitude,latitude,data_months,cmap=cmap,norm=norm) # plotting latitude
+
+    cbar= plt.colorbar(cs,ticks=uneven_levels,extend='max')
+   
+    cbar.set_label('Lifetime (day)')
+    plt.title('Chemical lifetime of 14CO in 2011 #{} at 550mb'.format(i))
+    plt.xlabel('Latitude')
+    plt.ylabel('Longitude')
+    plt.savefig('ChemLoss/Surface/14C_chemloss550_11_{}.png'.format(i))
     plt.show()
     
-    # #defining the tropopause level - only works for pressure plot 
-    # Tropopause = concen_month(tropo,i)    
-    # trop_level = 1000 * np.exp( -Tropopause.data/ 8.5e3) # converting tropopause from m to hBar    
     
-    # #using first order approximation to find pressure 
-    # pressure = 1000 * np.exp( -altitude / 8.5e3)
-    # plt.pcolor(latitude,pressure, data_months,vmax=10) # plotting latitude
+#%% Calcualting chemical lifetime IN 2010 
+for i in range(1,13):
     
-    # plt.plot(latitude,trop_level, color='black', label='Tropopause') # plot ropopause 
-    # plt.gca().invert_yaxis() # invert axis as max pressure at surface 
-        
-    # plt.yticks([10,100,200,300,400,500,600,700,800,900,000])
-
-    # cbar = plt.colorbar()
-    # cbar.set_label('Lifetime (year)')
-    # plt.title('Chemical lifetime of 14CO in years #{}'.format(i))
-    # plt.xlabel('Latitude')
+    trop_month = tropo12.extract(iris.Constraint(time = lambda cell: cell.point.month == i))
+    coords_lt = ['longitude','time']
+    trop_mean = trop_month.collapsed(coords_lt,iris.analysis.MEAN)
+   
+    rconc = concen_month(CO14_12,i).data
+    rloss = concen_month(CO14_loss12,i)  
     
-    # plt.ylabel('Pressure (hbar)')
-    # #plt.ylabel('Altitude (km)')
-    # plt.legend(loc='lower left')
-    # plt.savefig('ChemLoss2012/14C_chemloss{}.png'.format(i))
-    # plt.show()
-#%%
-CO14_loss_surface = CO14_loss12.extract(iris.Constraint(model_level_number = lambda cell: cell == 85 ))
+    loss_months1 = (rloss.data*6.02e23*(24*60*60))/(vol_model12.data[i-1,:,:,0]*1e6)
+    
+    #calculating the lifetime 
+    data_months = rconc/loss_months1
 
-CO14_loss_surface_30S_60S = CO14_loss_surface.extract(iris.Constraint(latitude = lambda cell: 0<cell<60))
-CO14_loss_surface_30N_60N = CO14_loss_surface.extract(iris.Constraint(latitude = lambda cell: -60<cell<0))
-coords_30_60 = ['latitude','longitude']
+    #extracting latitude at mindpoint of each cell 
+    latitude = rloss.coord('latitude').points
+    altitude = (rloss.coord('level_height').points)
+    
+    #using first order approximation to find pressure 
+    pressure = 1000 * np.exp( -altitude / 8.5e3)
+    trop_level = 1000 * np.exp( -trop_mean.data/ 8.5e3) # converting tropopause from m to hBar
+    
+    uneven_levels = [10, 15,30, 60, 360,3.6e3,3.6e5]
+    cmap_rb = plt.get_cmap('Spectral')
+    colors = cmap_rb(np.linspace(0, 1, len(uneven_levels) - 1))
+    cmap, norm = mcolors.from_levels_and_colors(uneven_levels, colors)
 
-CO14_loss_surface_30N_60N_mean = CO14_loss_surface_30N_60N.collapsed(coords_30_60,
-                                                                 iris.analysis.MEAN,
-                                                                 weights = iris.analysis.cartography.area_weights(CO14_loss_surface_30N_60N))
+    plt.figure()
+    cs= plt.pcolormesh(latitude,pressure,data_months,cmap=cmap,norm=norm) # plotting latitude
+    cbar= plt.colorbar(cs,ticks=uneven_levels,extend='max')
+    plt.plot(latitude,trop_level, color='black', label='Tropopause') # plot ropopause 
+    plt.gca().invert_yaxis() # invert axis as max pressure at surface 
 
-CO14_loss_surface_30S_60S_mean = CO14_loss_surface_30S_60S.collapsed(coords_30_60,
-                                                                 iris.analysis.MEAN,
-                                                                 weights = iris.analysis.cartography.area_weights(CO14_loss_surface_30S_60S))
+    plt.yticks([10,100,200,300,400,500,600,700,800,900,1000])
 
-plt.figure(figsize = (25,12.5))
-plt.plot(CO14_loss_surface_30N_60N_mean.data.data *6.02e23,label='0N-60N')
-plt.plot(CO14_loss_surface_30S_60S_mean.data.data*6.02e23, label='60S-0N')
-
-plt.ylabel('$^{14}$CO Loss (molecules/s')
-plt.xlabel('Days in 2010')
-plt.title('$^{14}$CO Loss at surface for everyday in 2010')
-plt.legend()
-plt.ylim(bottom = 0.0)
-plt.show()
-
-
-
+    cbar.set_label('Lifetime (day)')
+    plt.title('Chemical lifetime in 2012 # %.0f ' %(i))#,#np.max(data_months)/360))
+    plt.xlabel('Latitude')
+    plt.ylabel('Pressure (hbar)')
+    plt.legend()
+    plt.savefig('ChemLoss/ChemLoss2012/14C_chemloss{}.png'.format(i))
+    plt.show()
